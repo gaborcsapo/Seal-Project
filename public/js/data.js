126ef0548c48ce381cfa5b63ed914f2d3cb905b0
Gabor <csgabor14@gmail.com>
2016-12-03 01:00:37 +0800

    loading in correct order

--- public/js/data.js
+++ public/js/data.js
@@ -1,7 +1,7 @@
-var file1, file2, file3;
+var files = [];
 
 // sdata combines all data files and contains objects for each depth measurement
-var sdata = new Array();
+var sdata = [];
 
 function dataPoint(data, depth, temp, sal) {
     this.ref = data.ref;
@@ -15,136 +15,94 @@ function dataPoint(data, depth, temp, sal) {
     this.SAL = sal;
 }
 
-// load data year 12
-d3.csv("rs14years12.csv", function(data) {
-    file1 = data;
-    data.forEach(function(d) {
-        d.PTT = +d.PTT;
-        d.MAX_DBAR = +d.MAX_DBAR;
-        d.LAT = +d.LAT;
-        d.LON = +d.LON;
-        d.NUM = +d.NUM;
-        d.N_TEMP = +d.N_TEMP;
-        d.N_COND = +d.N_COND;
-        d.N_SAL = +d.N_SAL;
-        //Temperature at different depths
-        d.TEMP_DBAR = d.TEMP_DBAR.split(",");
-        for (var i = 0; i < d.TEMP_DBAR.length; i++) {
-            d.TEMP_DBAR[i] = +d.TEMP_DBAR[i];
-        }
-        d.TEMP_VALS = d.TEMP_VALS.split(",");
-        for (var i = 0; i < d.TEMP_VALS.length; i++) {
-            d.TEMP_VALS[i] = +d.TEMP_VALS[i];
-        }
-        //Salinity at different depths
-        d.SAL_DBAR = d.SAL_DBAR.split(",");
-        for (var i = 0; i < d.SAL_DBAR.length; i++) {
-            d.SAL_DBAR[i] = +d.SAL_DBAR[i];
-        }
-        d.SAL_VALS = d.SAL_VALS.split(",");
-        for (var i = 0; i < d.SAL_VALS.length; i++) {
-            d.SAL_VALS[i] = +d.SAL_VALS[i];
-        }
-        //Sal_CORRECTED_VALS
-        d.SAL_CORRECTED_VALS = d.SAL_CORRECTED_VALS.split(",");
-        for (var i = 0; i < d.SAL_CORRECTED_VALS.length; i++) {
-            d.SAL_CORRECTED_VALS[i] = +d.SAL_CORRECTED_VALS[i];
-        }
+function createDataPoint(data){
+    return new Promise(function (resolve, reject) {
+        files.push(data);
+        data.forEach(function(d) {
 
-        //Add objects for the various depths into sdata array
-        for (var i = 0; i < d.N_TEMP; i++) {
-            point = new dataPoint(d, d.TEMP_DBAR[i], d.TEMP_VALS[i], d.SAL_VALS[i]);
-            sdata.push(point);
-        }
+            // DELETE CAST TO NUMBER EVERYTHINg!!!!!!!!!!!!!!!!!
+            d.LAT = Number(d.LAT);
+            d.LON = Number(d.LON);
+            // DELETE CAST TO NUMBER EVERYTHINg!!!!!!!!!!!!!!!!!
+
+            d.PTT = +d.PTT;
+            d.MAX_DBAR = +d.MAX_DBAR;
+            d.LAT = +d.LAT;
+            d.LON = +d.LON;
+            d.NUM = +d.NUM;
+            d.N_TEMP = +d.N_TEMP;
+            d.N_COND = +d.N_COND;
+            d.N_SAL = +d.N_SAL;
+            //Temperature at different depths
+            d.TEMP_DBAR = d.TEMP_DBAR.split(",");
+            for (var i = 0; i < d.TEMP_DBAR.length; i++) {
+                d.TEMP_DBAR[i] = +d.TEMP_DBAR[i];
+            }
+            d.TEMP_VALS = d.TEMP_VALS.split(",");
+            for (var i = 0; i < d.TEMP_VALS.length; i++) {
+                d.TEMP_VALS[i] = +d.TEMP_VALS[i];
+            }
+            //Salinity at different depths
+            d.SAL_DBAR = d.SAL_DBAR.split(",");
+            for (var i = 0; i < d.SAL_DBAR.length; i++) {
+                d.SAL_DBAR[i] = +d.SAL_DBAR[i];
+            }
+            d.SAL_VALS = d.SAL_VALS.split(",");
+            for (var i = 0; i < d.SAL_VALS.length; i++) {
+                d.SAL_VALS[i] = +d.SAL_VALS[i];
+            }
+            //Sal_CORRECTED_VALS
+            d.SAL_CORRECTED_VALS = d.SAL_CORRECTED_VALS.split(",");
+            for (var i = 0; i < d.SAL_CORRECTED_VALS.length; i++) {
+                d.SAL_CORRECTED_VALS[i] = +d.SAL_CORRECTED_VALS[i];
+            }
+
+            //Add objects for the various depths into sdata array
+            for (var i = 0; i < d.N_TEMP; i++) {
+                point = new dataPoint(d, d.TEMP_DBAR[i], d.TEMP_VALS[i], d.SAL_VALS[i]);
+                sdata.push(point);
+            }
+        });
+        console.log('file loaded')
     });
+}
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('d3-request')) :
+  typeof define === 'function' && define.amd ? define(['d3-request'], factory) :
+  (global.d3 = global.d3 || {}, global.d3.promise = factory(global.d3));
+}(this, (function (d3) { 'use strict';
+
+function promisify(caller, fn) {
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return new Promise(function (resolve, reject) {
+      var callback = function callback(error, data) {
+        if (error) {
+          reject(Error(error));
+          return;
+        }
+        resolve(data);
+      };
+      fn.apply(caller, args.concat(callback));
+    });
+  };
+}
+
+var module$1 = {};
+
+['csv', 'tsv', 'json', 'xml', 'text', 'html'].forEach(function (fnName) {
+  module$1[fnName] = promisify(d3, d3[fnName]);
 });
 
-// load data year 13
-d3.csv("rs19years13.csv", function(data) {
-    file2 = data;
-    data.forEach(function(d) {
-        d.PTT = +d.PTT;
-        d.MAX_DBAR = +d.MAX_DBAR;
-        d.LAT = +d.LAT;
-        d.LON = +d.LON;
-        d.NUM = +d.NUM;
-        d.N_TEMP = +d.N_TEMP;
-        d.N_COND = +d.N_COND;
-        d.N_SAL = +d.N_SAL;
-        //Temperature at different depths
-        d.TEMP_DBAR = d.TEMP_DBAR.split(",");
-        for (var i = 0; i < d.TEMP_DBAR.length; i++) {
-            d.TEMP_DBAR[i] = +d.TEMP_DBAR[i];
-        }
-        d.TEMP_VALS = d.TEMP_VALS.split(",");
-        for (var i = 0; i < d.TEMP_VALS.length; i++) {
-            d.TEMP_VALS[i] = +d.TEMP_VALS[i];
-        }
-        //Salinity at different depths
-        d.SAL_DBAR = d.SAL_DBAR.split(",");
-        for (var i = 0; i < d.SAL_DBAR.length; i++) {
-            d.SAL_DBAR[i] = +d.SAL_DBAR[i];
-        }
-        d.SAL_VALS = d.SAL_VALS.split(",");
-        for (var i = 0; i < d.SAL_VALS.length; i++) {
-            d.SAL_VALS[i] = +d.SAL_VALS[i];
-        }
-        //Sal_CORRECTED_VALS
-        d.SAL_CORRECTED_VALS = d.SAL_CORRECTED_VALS.split(",");
-        for (var i = 0; i < d.SAL_CORRECTED_VALS.length; i++) {
-            d.SAL_CORRECTED_VALS[i] = +d.SAL_CORRECTED_VALS[i];
-        }
+return module$1;
 
-        //Add objects for the various depths into sdata array
-        for (var i = 0; i < d.N_TEMP; i++) {
-            point = new dataPoint(d, d.TEMP_DBAR[i], d.TEMP_VALS[i], d.SAL_VALS[i]);
-            sdata.push(point);
-        }
-    });
-});
+})));
 
-// load data year 14-15
-d3.csv("rs23years14-15.csv", function(data) {
-    file3 = data;
-    data.forEach(function(d) {
-        d.PTT = +d.PTT;
-        d.MAX_DBAR = +d.MAX_DBAR;
-        d.LAT = +d.LAT;
-        d.LON = +d.LON;
-        d.NUM = +d.NUM;
-        d.N_TEMP = +d.N_TEMP;
-        d.N_COND = +d.N_COND;
-        d.N_SAL = +d.N_SAL;
-        //Temperature at different depths
-        d.TEMP_DBAR = d.TEMP_DBAR.split(",");
-        for (var i = 0; i < d.TEMP_DBAR.length; i++) {
-            d.TEMP_DBAR[i] = +d.TEMP_DBAR[i];
-        }
-        d.TEMP_VALS = d.TEMP_VALS.split(",");
-        for (var i = 0; i < d.TEMP_VALS.length; i++) {
-            d.TEMP_VALS[i] = +d.TEMP_VALS[i];
-        }
-        //Salinity at different depths
-        d.SAL_DBAR = d.SAL_DBAR.split(",");
-        for (var i = 0; i < d.SAL_DBAR.length; i++) {
-            d.SAL_DBAR[i] = +d.SAL_DBAR[i];
-        }
-        d.SAL_VALS = d.SAL_VALS.split(",");
-        for (var i = 0; i < d.SAL_VALS.length; i++) {
-            d.SAL_VALS[i] = +d.SAL_VALS[i];
-        }
-        //Sal_CORRECTED_VALS
-        d.SAL_CORRECTED_VALS = d.SAL_CORRECTED_VALS.split(",");
-        for (var i = 0; i < d.SAL_CORRECTED_VALS.length; i++) {
-            d.SAL_CORRECTED_VALS[i] = +d.SAL_CORRECTED_VALS[i];
-        }
 
-        //Add objects for the various depths into sdata array
-        for (var i = 0; i < d.N_TEMP; i++) {
-            point = new dataPoint(d, d.TEMP_DBAR[i], d.TEMP_VALS[i], d.SAL_VALS[i]);
-            sdata.push(point);
-        }
-    });
-});
-
-console.log(sdata);
+let promiseFile1 = d3.promise.csv("rs14years12.csv");
+let promiseFile2 = d3.promise.csv("rs19years13.csv");
+let promiseFile3 = d3.promise.csv("rs23years14-15.csv");
--- public/js/map.js
+++ public/js/map.js
@@ -6,7 +6,7 @@ var shape_list = [];
 var containQuery;
 var putLocations;
 var location_list = [];
-
+var putLocations = function(){};
 
 function clearSelection () {
     if (selectedShape) {
@@ -35,154 +35,142 @@ function deleteSelectedShape () {
         
 }
 
-function initMap () {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 69.184529, lng: -50.462265},
-        zoom: 8,
-        mapTypeId: 'satellite',
-        streetViewControl: false
-    });
-    
-    var markerImage = new google.maps.MarkerImage('img/marker.png',
-            new google.maps.Size(10, 10),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(5, 5));
-    var locationImage = new google.maps.MarkerImage('img/location.png',
-            new google.maps.Size(5, 5),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(2.5, 2.5));
-    
-    var polyOptions = {
-        strokeWeight: 0,
-        fillOpacity: 0.45,
-        editable: true,
-        draggable: true
-    };
-    // Creates a drawing manager attached to the map that allows the user to draw
-    // markers, lines, and shapes.
-    drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYGON,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_CENTER,
-            drawingModes: ['marker', 'polygon']
-          },
-        markerOptions: {
-            icon: markerImage,
-            draggable: true
-        },
-        polylineOptions: {
+function finishedLoading(){
+    let promiseMap = new Promise((resolve, reject) => {
+        console.log(2);
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 69.184529, lng: -50.462265},
+            zoom: 8,
+            mapTypeId: 'satellite',
+            streetViewControl: false
+        });
+        
+        var markerImage = new google.maps.MarkerImage('img/marker.png',
+                new google.maps.Size(10, 10),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(5, 5));
+        var locationImage = new google.maps.MarkerImage('img/location.png',
+                new google.maps.Size(5, 5),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(2.5, 2.5));
+        
+        var polyOptions = {
+            strokeWeight: 0,
+            fillOpacity: 0.45,
             editable: true,
             draggable: true
-        },
-        polygonOptions: polyOptions,
-        map: map
-    });
-
-    google.maps.event.addListener(drawingManager, 'overlaycomplete', function (e) {
-        var newShape = e.overlay; 
-        newShape.type = e.type;
-        shape_list.push(newShape);
-        
-        if (e.type !== google.maps.drawing.OverlayType.MARKER) {
-            // Switch back to non-drawing mode after drawing a shape.
-            drawingManager.setDrawingMode(null);
-            // Add an event listener that selects the newly-drawn shape when the user mouses down on it.
-            google.maps.event.addListener(newShape, 'click', function (e) {
-                if (e.vertex !== undefined) {
-                    if (newShape.type === google.maps.drawing.OverlayType.POLYGON) {
-                        var path = newShape.getPaths().getAt(e.path);
-                        path.removeAt(e.vertex);
-                        if (path.length < 3) 
-                            newShape.setMap(null);
-                    }
-                    if (newShape.type === google.maps.drawing.OverlayType.POLYLINE) {
-                        var path = newShape.getPath();
-                        path.removeAt(e.vertex);
-                        if (path.length < 2) 
-                            newShape.setMap(null);
-                    }
-                }
-                setSelection(newShape);
-            });
-            setSelection(newShape);
-        }
-        else {
-            google.maps.event.addListener(newShape, 'click', function (e) {
-                setSelection(newShape);
-            });
-            setSelection(newShape);
-        }
-    });
-    //Only allow 2 markers to be on the map
-    google.maps.event.addListener(drawingManager, 'markercomplete', function (e) {
-        marker_list.push(e);
-        if (marker_list.length >2){
-            marker_list[0].setMap(null);
-            marker_list.shift();
-        }
-    })
-    
-    // Clear the current selection when the drawing mode is changed, or when the
-    // map is clicked.
-    google.maps.event.addListener(drawingManager, 'drawingmode_changed', clearSelection);
-    google.maps.event.addListener(map, 'click', clearSelection);
-    google.maps.event.addDomListener(document.getElementById('delete-button'), 'click', deleteSelectedShape);
-
-    containQuery = function(mylong, mylat){
-        for (var i=0; i<shape_list.length; i++){
-            if (google.maps.geometry.poly.containsLocation(new google.maps.LatLng({lat: mylat, lng: mylong}), shape_list[i])){
-                console.log("Contained");
-                break;
-            } else {
-                console.log("Not contained");
-            }
-        }  
-    }
-
-    putLocations = function(){
-        var singleMarker;
-        var markers = locations.map(function(location, i) {
-            singleMarker = new google.maps.Marker({
-                position: location,
-                icon: locationImage,
-                title: location.id
-            });
-            google.maps.event.addListener(singleMarker, 'click', function (e) {
-                updateIndividual(singleMarker.title);
-            })
-            return singleMarker;
+        };
+        // Creates a drawing manager attached to the map that allows the user to draw
+        // markers, lines, and shapes.
+        drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: google.maps.drawing.OverlayType.POLYGON,
+            drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: ['marker', 'polygon']
+            },
+            markerOptions: {
+                icon: markerImage,
+                draggable: true
+            },
+            polylineOptions: {
+                editable: true,
+                draggable: true
+            },
+            polygonOptions: polyOptions,
+            map: map
         });
-        // Add a marker clusterer to manage the markers.
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-    }
 
-    var locations = [
-    {lat: -31.563910, lng: 147.154312,id: "1"},
-    {lat: -33.718234, lng: 150.363181,id: "2"},
-    {lat: -33.727111, lng: 150.371124,id: "3"},
-    {lat: -33.848588, lng: 151.209834,id: "4"},
-    {lat: -33.851702, lng: 151.216968,id: "5"},
-    {lat: -34.671264, lng: 150.863657,id: "6"},
-    {lat: -35.304724, lng: 148.662905,id: "7"},
-    {lat: -36.817685, lng: 175.699196,id: "8"},
-    {lat: -36.828611, lng: 175.790222,id: "9"},
-    {lat: -37.750000, lng: 145.116667,id: "99"},
-    {lat: -37.759859, lng: 145.128708,id: "45"},
-    {lat: -37.765015, lng: 145.133858,id: "34"},
-    {lat: -37.770104, lng: 145.143299,id: "23"},
-    {lat: -37.773700, lng: 145.145187,id: "12"},
-    {lat: -37.774785, lng: 145.137978,id: "15"},
-    {lat: -37.819616, lng: 144.968119,id: "12"},
-    {lat: -38.330766, lng: 144.695692,id: "13"},
-    {lat: -39.927193, lng: 175.053218,id: "14"},
-    {lat: -41.330162, lng: 174.865694,id: "15"},
-    {lat: -42.734358, lng: 147.439506,id: "16"},
-    {lat: -42.734358, lng: 147.501315,id: "17"},
-    {lat: -42.735258, lng: 147.438000,id: "18"},
-    {lat: -43.999792, lng: 170.463352,id: "19"}
-    ]
+        google.maps.event.addListener(drawingManager, 'overlaycomplete', function (e) {
+            var newShape = e.overlay; 
+            newShape.type = e.type;
+            shape_list.push(newShape);
+            
+            if (e.type !== google.maps.drawing.OverlayType.MARKER) {
+                // Switch back to non-drawing mode after drawing a shape.
+                drawingManager.setDrawingMode(null);
+                // Add an event listener that selects the newly-drawn shape when the user mouses down on it.
+                google.maps.event.addListener(newShape, 'click', function (e) {
+                    if (e.vertex !== undefined) {
+                        if (newShape.type === google.maps.drawing.OverlayType.POLYGON) {
+                            var path = newShape.getPaths().getAt(e.path);
+                            path.removeAt(e.vertex);
+                            if (path.length < 3) 
+                                newShape.setMap(null);
+                        }
+                        if (newShape.type === google.maps.drawing.OverlayType.POLYLINE) {
+                            var path = newShape.getPath();
+                            path.removeAt(e.vertex);
+                            if (path.length < 2) 
+                                newShape.setMap(null);
+                        }
+                    }
+                    setSelection(newShape);
+                });
+                setSelection(newShape);
+            }
+            else {
+                google.maps.event.addListener(newShape, 'click', function (e) {
+                    setSelection(newShape);
+                });
+                setSelection(newShape);
+            }
+        });
+        //Only allow 2 markers to be on the map
+        google.maps.event.addListener(drawingManager, 'markercomplete', function (e) {
+            marker_list.push(e);
+            if (marker_list.length >2){
+                marker_list[0].setMap(null);
+                marker_list.shift();
+            }
+        })
+        
+        // Clear the current selection when the drawing mode is changed, or when the
+        // map is clicked.
+        google.maps.event.addListener(drawingManager, 'drawingmode_changed', clearSelection);
+        google.maps.event.addListener(map, 'click', clearSelection);
+        google.maps.event.addDomListener(document.getElementById('delete-button'), 'click', deleteSelectedShape);
 
-    putLocations();
+        containQuery = function(mylong, mylat){
+            for (var i=0; i<shape_list.length; i++){
+                if (google.maps.geometry.poly.containsLocation(new google.maps.LatLng({lat: mylat, lng: mylong}), shape_list[i])){
+                    console.log("Contained");
+                    break;
+                } else {
+                    console.log("Not contained");
+                }
+            }  
+        }
+
+        putLocations = function(){
+            console.log(3)
+            var singleMarker;
+            var markers = sdata.map(function(location, i) {
+                singleMarker = new google.maps.Marker({
+                    position: {'lat':location.LAT, 'lng':location.LON},
+                    icon: locationImage,
+                    title: location.ref
+                });
+                google.maps.event.addListener(singleMarker, 'click', function (e) {
+                    updateIndividual(singleMarker.title);
+                })
+                return singleMarker;
+            });
+            console.log(markers);
+                
+            // Add a marker clusterer to manage the markers.
+            var markerCluster = new MarkerClusterer(map, markers,
+                {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+        }     
+    });
+    console.log('1');
+    
+    promiseMap
+    .then(promiseFile1
+        .then(function(data){return createDataPoint(data)
+            .then(promiseFile2
+                .then(function(data){return createDataPoint(data)
+                    .then(promiseFile3
+                        .then(function(data){return createDataPoint(data)
+                            .then(putLocations())}))}))}));
+    
 }
-
