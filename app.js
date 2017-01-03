d6b48685cc62350c4ca7a3646e597adc34db7748
Gabor <csgabor14@gmail.com>
2017-01-03 09:42:32 -0500

    added paper

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.set("views", __dirname);
 app.engine('.html', require('ejs').__express);
@@ -11,7 +12,15 @@ var port = process.env.PORT || 3000;
 //ROUTES
 app.get('/instruction', function (reqqest, response) {
   response.render('instruction.html');
-})
+});
+
+app.get('/paper', function (reqqest, res) {
+    var filePath = "/public/paper.pdf";
+    fs.readFile(__dirname + filePath , function (err,data){
+        res.contentType("application/pdf");
+        res.send(data);
+    });
+});
 
 app.get("*", function(request, response){
 	response.render('index');
